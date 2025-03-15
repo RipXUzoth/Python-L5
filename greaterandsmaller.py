81b8cd569bcfc2390c51fa780ddcd0d6e334955e
RipXUzoth <akkuharish1@gmail.com>
2025-03-15 17:54:14 +0530

    greaterandsmaller

---
+++ greaterandsmaller.py
@@ -0,0 +1,8 @@
+i = int(input("enter a number:"))
+if (i < 15):
+    print("i is smaller than 15")
+    print("im in if Block")
+else:
+    print("i is grater than 15")
+    print("im in else block")
+print("im not in if or else Block")
